329a589fbe24f9ef586e1d2fc9bd1c3f1e922113
Till Prochaska <mail@tillprochaska.de>
2021-04-19 10:45:26 +0200

    Send optional payload with IFTTT webhooks

--- blocks/ifttt_publish.js
+++ blocks/ifttt_publish.js
@@ -5,23 +5,17 @@ const __{{NAMESPACE}}_ifttt_publish_json = {
     "previousStatement": null,
     "nextStatement": null,
 
-    "message0": "%1",
-    "args0": [
-        {
-            "type": "field_label",
-            "text": "Trigger IFTTT webhook",
-        },
-    ],
+    "inputsInline": true,
 
-    "message1": "%1 %2",
-    "args1": [
-        {
-            "type": "field_label",
-            "text": "Event",
-        },
+    "message0": "Trigger IFTTT webhook %1 with data %2",
+    "args0": [
         {
             "type": "field_input",
             "name": "EVENT"
+        },
+        {
+            "type": "input_value",
+            "name": "DATA"
         },
     ],
 };
@@ -34,15 +28,17 @@ window.Blockly.Blocks['__{{NAMESPACE}}_ifttt_publish'] = {
 
 window.Blockly.Python['__{{NAMESPACE}}_ifttt_publish'] = function(block) {
     const event = block.getFieldValue('EVENT');
+    const data = Blockly.Python.valueToCode(block, 'DATA', Blockly.Python.ORDER_NONE);
 
     // Import HTTP client library
     window.Blockly.Python.definitions_['tactilenews_ifttt_publish_import'] = 'import urequests\n';
 
     let code = '';
     code += `try:\n`;
+    code += `  data = str(${data || '\'\''})\n`;
     code += `  key = _tactilenews_ifttt['key']\n`;
     code += `  url = 'https://maker.ifttt.com/trigger/${event}/with/key/' + key\n`;
-    code += `  req = urequests.request(method='POST', url=url, headers={})\n`;
+    code += `  req = urequests.request(method='POST', url=url, headers={}, json={'value1': ${data}})\n`;
     code += `except:\n`;
     code += `  pass\n`;
 
